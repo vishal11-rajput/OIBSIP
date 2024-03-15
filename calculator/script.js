9c4e1b28f9d3cfb4c2c869ea396a8d7b76d0fccd
vishal11-rajput <vishalofficial787@gmail.com>
2024-03-15 09:33:32 +0530

    calculator and tribute project
    added project files

---
+++ calculator/script.js
@@ -0,0 +1,31 @@
+
+let output = "";
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+
+
+//calculate
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    
+    output = output.toString().slice(0, -1);
+  } else {
+    
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+//eventlistener
+buttons.forEach((button) => {
+  
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
